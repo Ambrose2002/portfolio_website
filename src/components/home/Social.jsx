07fa67bd117dcfc22667cb2c435399c15892709e
Ambrose Blay <ambrose2002blay@gmail.com>
2023-12-28 14:38:20 -0500

    home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/ambrose_blay/" className="home__social-icon" target="_blank">
+            <i class="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/ambrose-blay/" className="home__social-icon" target="_blank">
+            <i class="uil uil-linkedin"></i>
+            </a>
+
+            <a href="https://github.com/Ambrose2002" className="home__social-icon" target="_blank">
+            <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
