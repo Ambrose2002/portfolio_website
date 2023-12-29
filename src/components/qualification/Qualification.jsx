2df824987ac3c1808db450feedb1888a2895ed85
Ambrose Blay <ambrose2002blay@gmail.com>
2023-12-29 13:50:43 -0500

    start qualification

---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './qualification.css'
+
+const Qualification = () => {
+    return (
+        <section className="qualification section">
+            <h2 className="section__title">Qualification</h2>
+            <span className="section__subtitle">My Personal Journey</span>
+
+            <div className="qualification__container container"></div>
+        </section>
+    )
+}
+
+export default Qualification
