8cbef3d7ea1ac57fe0ab979888cc55478b01a61c
Ambrose Blay <ambrose2002blay@gmail.com>
2023-12-30 19:11:37 -0500

    loop video

--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -7,14 +7,9 @@ import Work6 from "../../assets/connectn.mp4"
 const Works = () => {
     return (
         <div className="work__container container grid">
-            {
-                projectsData.map((item) => {
-                    return <WorksItems item={item} key={item.id} link={item.link} />
-                })
-            }
             <div className="work__card" key="5">
-                <video className='work__img' height="190px" playSinline autoPlay muted>
-                    <source src={Work5} type='video/mp4'/>
+                <video className='work__img' height="190px" playSinline autoPlay muted loop>
+                    <source src={Work5} type='video/mp4' />
                 </video>
                 <h3 className="work__title">Invaders</h3>
                 <a href="https://github.com/Ambrose2002/Invaders" className="work__button" target='_blank'>
@@ -23,14 +18,19 @@ const Works = () => {
             </div>
 
             <div className="work__card" key="6">
-                <video className='work__img' height="190px"  playSinline autoPlay muted>
-                    <source src={Work6} type='video/mp4'/>
+                <video className='work__img' height="190px" playSinline autoPlay muted loop>
+                    <source src={Work6} type='video/mp4' />
                 </video>
                 <h3 className="work__title">ConnectN</h3>
                 <a href="https://github.com/Ambrose2002/ConnectN" className="work__button" target='_blank'>
                     Repo Link <i className="bx bx-right-arrow-alt work__button-icon"></i>
                 </a>
             </div>
+            {
+                projectsData.map((item) => {
+                    return <WorksItems item={item} key={item.id} link={item.link} />
+                })
+            }
         </div>
     )
 }
