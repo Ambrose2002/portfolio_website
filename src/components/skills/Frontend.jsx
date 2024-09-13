0451d9270d5fa8f655d71ae359e9a9e729c5ba05
Ambrose Blay <ambrose2002blay@gmail.com>
2024-09-13 13:33:31 -0400

    update resume

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -50,7 +50,7 @@ const Frontend = () => {
                         <i class='bx bx-badge-check'></i>
 
                         <div>
-                            <h3 className="skills__name">Git</h3>
+                            <h3 className="skills__name">Swift</h3>
                             <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
